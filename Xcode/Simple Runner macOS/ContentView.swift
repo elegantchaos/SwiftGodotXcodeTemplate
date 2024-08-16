45a7fbc04795fcfd178a6bab98a1f3036501e34d
Sam Deane <sam@elegantchaos.com>
2024-08-16 11:26:07 +0100

    experimenting with mac targets

---
+++ Xcode/Simple Runner macOS/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Simple Runner macOS
+//
+//  Created by Sam Deane on 16/08/2024.
+//  Copyright © 2024 GodotEngine. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xcode/Simple Runner macOS/Simple_Runner_macOSApp.swift
@@ -0,0 +1,18 @@
+//
+//  Simple_Runner_macOSApp.swift
+//  Simple Runner macOS
+//
+//  Created by Sam Deane on 16/08/2024.
+//  Copyright © 2024 GodotEngine. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Runner_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
